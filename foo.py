16dd00b34ee7637789d579c6d42055dbb8bfbc32
Simon Doppler <dopsi@dopsi.ch>
2024-02-11 14:08:22 +0100

    fix flake8 lint

--- foo.py
+++ foo.py
@@ -2,14 +2,18 @@ import sys
 import os
 import math
 
+
 def bar():
     print(sys.argv, os.getcwd())
+
 
 def baz() -> int:
     print("this is function baz")
 
+
 def zab() -> int:
     return math.pi
+
 
 if __name__ == "__main__":
     bar()
