3d8cbeac12ffc983dcf1b460e0848c5424bf1bb7
Simon Doppler <dopsi@dopsi.ch>
2024-02-11 13:49:07 +0100

    more silly python

--- foo.py
+++ foo.py
@@ -1,11 +1,15 @@
 import sys
 import os
+import math
 
 def bar():
     print(sys.argv, os.getcwd())
 
 def baz() -> int:
     print("baz")
+
+def zab() -> int:
+    return math.pi
 
 if __name__ == "__main__":
     bar()
